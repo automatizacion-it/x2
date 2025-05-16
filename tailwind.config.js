1c10a4916978491c88578270a48f268127508836
automatizacion-it <85306306+automatizacion-it@users.noreply.github.com>
2025-05-16 04:10:50 +0000

    DDDDDDDDDDDDDDDD

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fondoClaro: '#f3f4f6',
+        fondoOscuro: '#1f2937',
+        notaAmarilla: '#fef9c3',
+        notaVerde: '#d1fae5',
+        notaAzul: '#bfdbfe',
+        primario: '#3b82f6',
+        secundario: '#f87171',
+      },
+    },
   },
+  darkMode: 'class', // ✅ importante
   plugins: [],
 }
